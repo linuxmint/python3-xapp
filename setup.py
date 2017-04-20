f53ece6fd1c513db38a8d27d5f2d0ee4c522f9e3
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-04-20 18:11:24 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup( name = "python-xapp",
+       version = "1.0.0",
+       description = "Python Xapp Library.",
+       maintainer = "Linux Mint",
+       maintainer_email = "root@linuxmint.com",
+       url = "http://github.com/linuxmint/python-xapp",
+       packages = ['xapp'],
+       classifiers = [
+                "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+                "Programming Language :: Python :: 2.6",
+                "Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3",
+                "Topic :: Desktop Environment",
+                ],
+     )
+
---
+++ xapp/os.py
@@ -0,0 +1,105 @@
+from __future__ import absolute_import
+
+import os
+import psutil
+import subprocess
+
+### SESSION DETECTION
+
+SESSION_CINNAMON = "Cinnamon"
+SESSION_MATE = "MATE"
+SESSION_XFCE = "XFCE"
+SESSION_KDE = "KDE"
+SESSION_GNOME = "GNOME"
+SESSION_UNKNOWN = ""
+
+def get_current_desktop():
+
+    session = os.getenv("XDG_CURRENT_DESKTOP", SESSION_UNKNOWN)
+
+    for name in (SESSION_CINNAMON, SESSION_MATE, SESSION_XFCE, SESSION_KDE):
+        if session.lower() == name.lower():
+            return name
+
+    if session == "X-Cinnamon":
+        return SESSION_CINNAMON
+
+    return SESSION_UNKNOWN
+
+def is_desktop_cinnamon():
+    return get_current_desktop() == SESSION_CINNAMON
+
+def is_desktop_mate():
+    return get_current_desktop() == SESSION_MATE
+
+def is_desktop_xfce():
+    return get_current_desktop() == SESSION_XFCE
+
+def is_desktop_kde():
+    return get_current_desktop() == SESSION_KDE
+
+def is_desktop_gnome():
+    return get_current_desktop() == SESSION_GNOME
+
+### PROCESS DETECTION
+
+def is_process_running(process_name):
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        if p.name() == process_name:
+            return True
+    return False
+
+### POLKIT SUPPORT
+
+def is_polkit_running():
+
+    # Check that pkexec is present
+    if not os.path.exists("/usr/bin/pkexec"):
+        return False
+
+    # Check that the polkit agent is running
+    if is_desktop_kde() and is_process_running("polkit-kde-authentication-agent-1"):
+        return True
+    elif is_process_running("polkit-gnome-authentication-agent-1"):
+        return True
+    else:
+        return False
+
+def pkexec(command):
+    if not isinstance(command, list):
+        command = command.split(" ")
+
+    display = os.getenv("DISPLAY")
+    xauthority = os.getenv("XAUTHORITY")
+
+    subprocess.Popen(["pkexec", "env", "DISPLAY=%s" % display, "XAUTHORITY=%s" % xauthority] + command)
+    print(["pkexec", "env", "DISPLAY=%s" % display, "XAUTHORITY=%s" % xauthority] + command)
+
+### Run as root
+
+def run_with_admin_privs(command, message=None, icon=None, skip_pkexec=False):
+    if not isinstance(command, list):
+        command = command.split(" ")
+
+    if is_polkit_running() and not skip_pkexec:
+        pkexec(command)
+        return True
+    elif os.path.exists("/usr/bin/gksu"):
+        commands = ["gksu"]
+        if message is not None:
+            commands = commands + ["--message", "<b>%s</b>" % message]
+        commands = commands + command
+        subprocess.Popen(commands)
+        return True
+    elif os.path.exists("/usr/bin/kdesudo"):
+        commands = ["kdesudo", "-d"]
+        if icon is not None:
+            commands = commands + ["-i", icon]
+        if message is not None:
+            commands = commands + ["--comment", "<b>%s</b>" % message]
+        commands = commands + command
+        subprocess.Popen(commands)
+        return True
+    else:
+        return False
